16330238adc7cae77b7330d7e0e3396f2d9f1c88
aabbcc <aabbcc@aabbccdeMac-mini.local>
2017-05-22 14:11:26 +0800

    shang chuan

---
+++ tess/tess/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  tess
+//
+//  Created by aabbcc on 2017/5/22.
+//  Copyright © 2017年 aabbcc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"123321");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
